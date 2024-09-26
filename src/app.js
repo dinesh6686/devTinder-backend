ad46021b42180cb3bb5755ea0b1e063a15bd51b6
Dinesh Venkat <dinesh6686@gmail.com>
2024-09-26 13:20:18 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 7777;
+
+app.use(express.json());
+app.use('/hello',(req, res) => { 
+    res.send('Hi friends!!!');
+})
+app.use('/test', (req, res) => { 
+    res.send('Test endpointtt')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server up and running on port ${PORT}`)
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
