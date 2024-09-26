25a501a2f8fe971d051a52c668f4f07daf2bf7a2
Dinesh Venkat <dinesh6686@gmail.com>
2024-09-26 17:26:43 +0530

    playing around with routing and notes

--- src/app.js
+++ src/app.js
@@ -78,6 +78,34 @@ app.get('/users/:id?', (req, res) => {
     res.send()
 })
 
+app.get('/ab*cd', (req, res) => { 
+    res.send('Hello from ab?c')
+})
+
 // routing order matters
 // http methods: CRUD operations -> GET, POST, DELETE, PUT, PATCH
 
+
+// Advance routing patterns
+// ab?c  -> ac or abc -> b is optional
+// a(bc)?d -> ad or abcd -> bc is optional
+// ab+c -> abc or abbbbbbbbc   -> a(as many b s as you like)c
+// a(bc)+d -> abcd or abcbcbcbcd -> a(as many bc s as you like)d
+// ab*cd -> abjkhvhkljcd    -> starts with ab and ends with cd
+
+// users?id=123 -> id = req.query.id -> query parameters
+// users?id=123&name=Dinesh -> id = req.query.id, name = req.query.name -> query parameters
+
+// Dynamic routes(:)
+//   /users/:id -> id = req.params.id -> path parameters
+//   /users/:id/:name/:password -> id = req.params.id, name = req.params.name, password = req.params.password -> path parameters
+//   /users/:id? -> optional path parameters
+
+
+// regex routing patterns
+// /a/ -> if a leter is there in the path, /gfgfgfgdfga -> this will work
+// /.*fly$/ ->if endpoint starts with anything but ending with fly
+
+app.get(/.*fly$/, (req, res) => {
+    res.send('Hello from regex')
+})
