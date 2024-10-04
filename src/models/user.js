6560be9f042694262372f3162e0a2db5d6c27c18
Dinesh Venkat <dinesh6686@gmail.com>
2024-10-04 18:48:07 +0530

    exploring validator library and its methods

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -14,14 +15,24 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid Email address: ' + value)
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error('Enter a strong password: ' + value)
+            }
+        }
     },
     age: {
         type: Number,
-        min:18
+        min: 18
     },
     gender: {
         type: String,
@@ -33,6 +44,11 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl: {
         type: String,
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error('Invalid URL: ' + value)
+            }
+        }
     },
     about: {
         type: String,
